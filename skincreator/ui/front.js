c501e8defc21f19c1fd47703ccd571403423346a
Yannick <yannickdg99@gmail.com>
2019-07-31 18:24:35 +0200

    Last edit for language

--- skincreator/ui/front.js
+++ skincreator/ui/front.js
@@ -1,82 +1,87 @@
+// Set language
+$("#appearanceTab").html(scLocale.appearanceTab);
+$("#hairinessTab").html(scLocale.hairinessTab);
+$("#clothesTab").html(scLocale.clothesTab);
+
 // Put colors for input[type=radio]
-$('.color').each(function(){
+$('.color').each(function () {
   var color = $(this).attr('data-color');
   $(this).css('background-color', color);
 });
 
 // Arrows for input[type=range]
-$('.arrow-right').on('click', function(e){
+$('.arrow-right').on('click', function (e) {
   e.preventDefault();
   var value = parseFloat($(this).prev().val()),
-      newValue = parseFloat(value + 1),
-      max = $(this).parent().prev().attr('data-legend');
+    newValue = parseFloat(value + 1),
+    max = $(this).parent().prev().attr('data-legend');
   $(this).prev().val(newValue);
-  $(this).parent().prev().text(newValue+''+max);
+  $(this).parent().prev().text(newValue + '' + max);
 });
 
-$('.arrow-left').on('click', function(e){
+$('.arrow-left').on('click', function (e) {
   e.preventDefault();
   var value = parseFloat($(this).next().val()),
-      newValue = parseFloat(value - 1),
-      max = $(this).parent().prev().attr('data-legend');
+    newValue = parseFloat(value - 1),
+    max = $(this).parent().prev().attr('data-legend');
   $(this).next().val(newValue);
-  $(this).parent().prev().text(newValue+''+max);
+  $(this).parent().prev().text(newValue + '' + max);
 });
 
 // Arrows for clothes module
-$('.arrowvetement-right').on('click', function(e){
+$('.arrowvetement-right').on('click', function (e) {
   var li = $(this).parent().find('li.active'),
-      active = li.next(),
-      id = active.attr('data'),
-      max = $(this).parent().find('li:last-of-type').attr('data');
-  if($(this).prev().hasClass('active')){
+    active = li.next(),
+    id = active.attr('data'),
+    max = $(this).parent().find('li:last-of-type').attr('data');
+  if ($(this).prev().hasClass('active')) {
     li.removeClass('active');
     $(this).parent().find('li:first-of-type').addClass('active');
-    $(this).parent().parent().find('.label-value').text('0/'+max)
-  }else{
+    $(this).parent().parent().find('.label-value').text('0/' + max)
+  } else {
     li.removeClass('active');
     active.addClass('active');
-    $(this).parent().parent().find('.label-value').text(id+'/'+max)
+    $(this).parent().parent().find('.label-value').text(id + '/' + max)
   }
 });
-$('.arrowvetement-left').on('click', function(e){
+$('.arrowvetement-left').on('click', function (e) {
   var li = $(this).parent().find('li.active'),
-      active = li.prev(),
-      id = active.attr('data'),
-      max = $(this).parent().find('li:last-of-type').attr('data');
-  if($(this).next().hasClass('active')){
+    active = li.prev(),
+    id = active.attr('data'),
+    max = $(this).parent().find('li:last-of-type').attr('data');
+  if ($(this).next().hasClass('active')) {
     li.removeClass('active');
     $(this).parent().find('li:last-of-type').addClass('active');
-    $(this).parent().parent().find('.label-value').text(max+'/'+max)
-  }else{
+    $(this).parent().parent().find('.label-value').text(max + '/' + max)
+  } else {
     li.removeClass('active');
-    active.addClass('active');  
-    $(this).parent().parent().find('.label-value').text(id+'/'+max)
+    active.addClass('active');
+    $(this).parent().parent().find('.label-value').text(id + '/' + max)
   }
 });
 
 // Put value of input into label-value
-$('.input .label-value').each(function(){
+$('.input .label-value').each(function () {
   var max = $(this).attr('data-legend'),
-      val = $(this).next().find('input').val();
-  $(this).parent().find('.label-value').text(val+''+max);
+    val = $(this).next().find('input').val();
+  $(this).parent().find('.label-value').text(val + '' + max);
 });
-$('input[type=range]').change(function(){
+$('input[type=range]').change(function () {
   var value = parseFloat($(this).val()),
-      max = $(this).parent().prev().attr('data-legend');
-  $(this).parent().prev().text(value+''+max);
+    max = $(this).parent().prev().attr('data-legend');
+  $(this).parent().prev().text(value + '' + max);
 });
-$('.vetements .group').each(function(){
+$('.vetements .group').each(function () {
   var max = $(this).find('li:last-of-type').attr('data');
-  $(this).find('.label-value').text('0/'+max);
+  $(this).find('.label-value').text('0/' + max);
 });
 
 // Popup click on submit
-$('.submit').on('click', function(e){
+$('.submit').on('click', function (e) {
   e.preventDefault();
   $('.popup').fadeIn(200);
-}); 
-$('.popup .button').on('click', function(e){
+});
+$('.popup .button').on('click', function (e) {
   e.preventDefault();
   $('.popup').fadeOut(200);
   // Set language
--- skincreator/ui/locales/en.js
+++ skincreator/ui/locales/en.js
@@ -1,5 +1,59 @@
 var scLocale = new Object();
-scLocale.dropItem = "Drop";
-scLocale.useItem = "Use";
-scLocale.giveItem = "Give";
-scLocale.secondInventoryNotAvailable = "Second inventory is not available";
+scLocale.appearanceTab = "Appearance";
+scLocale.hairinessTab = "Hairiness";
+scLocale.clothesTab = "Clothing";
+scLocale.morphology = "Morphology";
+scLocale.maleFaces = "Male";
+scLocale.femaleFaces = "Female";
+scLocale.eyeColor = "Eye color";
+scLocale.skinHeader = "Skin";
+scLocale.skinColor = "Skin color";
+scLocale.wrinkles = "Wrinkles";
+scLocale.wrinkleIntensity = "Intensity of wrinkles";
+scLocale.hairHeader = "Hair";
+scLocale.hairstyle = "Hairstyle";
+scLocale.hairColor = "Hair color";
+scLocale.eyebrowHeader = "Eyebrows";
+scLocale.eyebrowShape = "Shape of eyebrow";
+scLocale.eyebrowThickness = "Thickness of the eyebrows";
+scLocale.beardHeader = "Beard";
+scLocale.beardStyle = "Beard style";
+scLocale.beardThickness = "Thickness of the beard";
+scLocale.beardColor = "Beard color";
+scLocale.hatsHeader = "Headwear";
+
+scLocale.headwear = [
+    "None",
+    "Black Fedora",
+    "White Fedora",
+    "Fisher hat",
+    "Black LS cap",
+    "White LS cap",
+    "Reggae hat",
+    "White beret",
+    "Grey beret",
+    "Black beret",
+    "Brown beret",
+    "Plain cap",
+    "Colored cap",
+    "Stank cap",
+    "Stank cap inversed",
+    "Gray hat",
+    "White hat",
+    "Cowboy hat",
+    "White bandana",
+    "Black bandana",
+    "Black audio headphones",
+    "White audio headphones",
+    "Cowboy hat",
+    "Holiday hat",
+    "Mafia hat",
+    "Melon shaped hat",
+    "Formal hat",
+    "Fairy ears",
+    "Red Broker Cap",
+    "Black Broker Cap",
+    "Trickster cap",
+    "Vapid cap",
+    "Christmas hat"
+];
