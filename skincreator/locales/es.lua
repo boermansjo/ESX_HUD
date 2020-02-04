ac5bebfae93bb1673d3036e57ee6ccff0fa8d166
Luis26x <60543200+Luis26x@users.noreply.github.com>
2020-02-04 09:59:44 -0500

    es.lua
    add spanish

---
+++ skincreator/locales/es.lua
@@ -0,0 +1,5 @@
+['skin_menu'] = 'Menu de piel',
+['use_rotate_view'] = 'Use ~INPUT_VEH_FLY_ROLL_IZQUIERDO_Solo~ y ~INPUT_VEH_FLY_ROLL_DEDERECHO_Solo~ para rotar la vista.',
+['skin'] = 'Cambio de piel',
+['saveskin'] = 'Guardar pier en un archivo',
+}
